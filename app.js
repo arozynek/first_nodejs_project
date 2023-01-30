8ceeff4edf67c5b6c872801d1f4cb5dca3eacca3
arozynek <panna.anna.r@gmail.com>
2023-01-30 20:33:05 +0100

    Add routes.js
    Cleanup using exports - moving part of the code from app.js to routes.js

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  // console.log(req.url, req.method, req.headers);
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter message</title></head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const reqBody = [];
-    req.on("data", (chunk) => {
-      reqBody.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(reqBody).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-    // res.writeHead();
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My first page</title></head>");
-  res.write("<body><h1>Hello from server node.js</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const routes = require("./routes");
+
+const server = http.createServer(routes.reqHandler);
+
 server.listen(3000);
