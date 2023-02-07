79cbf734ae42a649df05df5885f1489ccc99ff3f
arozynek <panna.anna.r@gmail.com>
2023-02-07 13:51:52 +0100

    Sending response with middleware

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
 const app = express();
-app.use((req, res, next) => {
-  console.log("The middleware");
-  next();
+
+app.use("/add-prod", (req, res, next) => {
+  res.send("<h1> Add product page</h1>");
 });
-app.use((req, res, next) => {
-  console.log("Another middleware");
+app.use("/", (req, res, next) => {
+  res.send("<h1> Another middleware</h1>");
 });
 
 app.listen(3000);
