0673f4e80fd29ce57184beda2e2f2777143aa1c0
arozynek <panna.anna.r@gmail.com>
2023-02-08 14:54:01 +0100

    Filter routes

--- routes/admin.js
+++ routes/admin.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 router.get("/add-prod", (req, res, next) => {
   res.send(
-    "<h1> Add product page</h1><form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
+    "<h1> Add product page</h1><form action='/admin/add-prod' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
   );
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-prod", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
