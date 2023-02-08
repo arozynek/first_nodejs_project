aed17840d0da5acd3f77e26cbfc11382e55e24bf
arozynek <panna.anna.r@gmail.com>
2023-02-08 12:40:12 +0100

    Routes added

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const bodyParser = require("body-parser");
+
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/add-prod", (req, res, next) => {
-  res.send(
-    "<h1> Add product page</h1><form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
-  );
-});
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-app.use("/", (req, res, next) => {
-  res.send("<h1>Home page</h1>");
-});
+
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
