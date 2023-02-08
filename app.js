4674575de9a89387ae167c88f0649be7335192c6
arozynek <panna.anna.r@gmail.com>
2023-02-08 11:37:07 +0100

    body-parser installed

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/add-prod", (req, res, next) => {
-  res.send("<h1> Add product page</h1>");
+  res.send(
+    "<h1> Add product page</h1><form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
+  );
+});
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 app.use("/", (req, res, next) => {
-  res.send("<h1> Another middleware</h1>");
+  res.send("<h1>Home page</h1>");
 });
 
 app.listen(3000);
