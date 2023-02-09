c597bac54201ff680b5ba911e71ef53fab4a28f0
arozynek <panna.anna.r@gmail.com>
2023-02-09 13:06:34 +0100

    Serving HTML pages

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,10 @@
+const path = require("path");
 const express = require("express");
 const router = express.Router();
+const rootDir = require("../helper/path");
 
 router.get("/", (req, res, next) => {
-  res.send("<h1>Home page</h1>");
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
