0522acb767cf9d3322242e68b0b0a07dfc25f6b2
arozynek <panna.anna.r@gmail.com>
2023-02-21 19:31:53 +0100

    Using template engines
    Learn about pug, handlebars and EJS

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const path = require("path");
 const router = express.Router();
-const rootDir = require("../helper/path");
 
+const products = [];
 router.get("/add-prod", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", { pageTitle: "Add products" });
 });
 router.post("/add-prod", (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,14 @@
-const path = require("path");
 const express = require("express");
 const router = express.Router();
-const rootDir = require("../helper/path");
+const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", {
+    pageTitle: "Shop",
+    prods: products,
+    hasProducts: products.length > 0,
+  });
 });
 
 module.exports = router;
